d1ca221f12d32bc3af5687852f2cd1db0723869d
Alexander Cleoni <alexander.cleoni@gmail.com>
2018-06-18 14:21:10 -0400

    Posts render on screen through map function.

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,16 +2,30 @@ import React, { Component } from 'react';
 import  { URL } from '../api/jsonplacehoder'
 
 class Posts extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            posts: []
+        }
+    }
 
     componentWillMount() {
         fetch(`${URL}/posts`)
             .then(res => res.json())
-            .then(data => console.log(data));
+            .then(data => this.setState({posts: data}));
     }
     render() {
+        const PostInfo = this.state.posts.map(post => (
+            <div key={post.id}>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+            </div>
+
+        ))
         return (
             <div>
                 <h1>Posts</h1>
+                {PostInfo}
             </div>
         )
     }
