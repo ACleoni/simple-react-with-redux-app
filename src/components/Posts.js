b047b7a4181965f500657bfef35535ea20d69ff7
Alexander Cleoni <alexander.cleoni@gmail.com>
2018-06-18 14:15:44 -0400

    Adds fake data api

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,7 +1,13 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
+import  { URL } from '../api/jsonplacehoder'
 
 class Posts extends Component {
+
+    componentWillMount() {
+        fetch(`${URL}/posts`)
+            .then(res => res.json())
+            .then(data => console.log(data));
+    }
     render() {
         return (
             <div>
